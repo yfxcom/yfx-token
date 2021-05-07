a3a6699963674a26d9387c05f4f0d434c71999b1
whale <zgndsl@gmail.com>
2021-05-07 17:36:26 +0800

    multi send

--- contracts/MultiSender.sol
+++ contracts/MultiSender.sol
@@ -3,6 +3,31 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+library TransferHelper {
+    function safeApprove(address token, address to, uint value) internal {
+        // bytes4(keccak256(bytes('approve(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: APPROVE_FAILED');
+    }
+
+    function safeTransfer(address token, address to, uint value) internal {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FAILED');
+    }
+
+    function safeTransferFrom(address token, address from, address to, uint value) internal {
+        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FROM_FAILED');
+    }
+
+    function safeTransferETH(address to, uint value) internal {
+        (bool success,) = to.call{value:value}(new bytes(0));
+        require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
+    }
+}
+
 contract MultiSender {
 
     using SafeMath for uint;
@@ -15,12 +40,12 @@ contract MultiSender {
         uint sendAmount = _to.length.sub(1).mul(_value);
         uint remainingValue = msg.value;
 
-        require(remainingValue >= sendAmount);
-		require(_to.length <= 255);
+        require(remainingValue >= sendAmount, "eth not enough");
+		require(_to.length <= 255, "too many");
 
 		for (uint8 i = 1; i < _to.length; i++) {
 			remainingValue = remainingValue.sub(_value);
-			require(_to[i].send(_value));
+			require(_to[i].send(_value), "eth send fail");
 		}
 
         emit EthMultiSent(msg.value);
@@ -31,14 +56,14 @@ contract MultiSender {
         uint sendAmount = _value[0];
 		uint remainingValue = msg.value;
 
-        require(remainingValue >= sendAmount);
+        require(remainingValue >= sendAmount, "eth not enough");
 
-		require(_to.length == _value.length);
-		require(_to.length <= 255);
+		require(_to.length == _value.length, "lenght not equal");
+		require(_to.length <= 255, "too many");
 
 		for (uint8 i = 1; i < _to.length; i++) {
 			remainingValue = remainingValue.sub(_value[i]);
-			require(_to[i].send(_value[i]));
+			require(_to[i].send(_value[i]), "eth send fail");
 		}
 
         emit EthMultiSent(msg.value);
@@ -46,28 +71,34 @@ contract MultiSender {
 
     function coinSendSameValue(address _tokenAddress, address[] calldata _to, uint _value)  public {
 
-		require(_to.length <= 255);
+		require(_to.length <= 255, "too many");
 		
 		address from = msg.sender;
 		uint256 sendAmount = _to.length.sub(1).mul(_value);
 
         IERC20 token = IERC20(_tokenAddress);
+
+        require(token.balanceOf(from) >= sendAmount, "balance not enough");
+
 		for (uint8 i = 1; i < _to.length; i++) {
-			token.transferFrom(from, _to[i], _value);
+            TransferHelper.safeTransferFrom(from, _to[i], _value);
 		}
 
         emit TokenMultiSent(_tokenAddress, sendAmount);
 	}
 
 	function coinSendDifferentValue(address _tokenAddress, address[] calldata _to, uint[] calldata _value)  public  {
-		require(_to.length == _value.length);
-		require(_to.length <= 255);
+		require(_to.length == _value.length, "lenght not equal");
+		require(_to.length <= 255, "too many");
 
         uint256 sendAmount = _value[0];
+        
         IERC20 token = IERC20(_tokenAddress);
+
+        require(token.balanceOf(from) >= sendAmount, "balance not enough");
         
 		for (uint8 i = 1; i < _to.length; i++) {
-			token.transferFrom(msg.sender, _to[i], _value[i]);
+            TransferHelper.safeTransferFrom(msg.sender, _to[i], _value[i]);
 		}
 
         emit TokenMultiSent(_tokenAddress, sendAmount);
